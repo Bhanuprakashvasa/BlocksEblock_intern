e4fc3b79c8c06f4565a80c7f3f808b02a03cd3dd
mr_bhanu__33 <115440020+Bhanuprakashvasa@users.noreply.github.com>
2025-06-09 11:51:34 +0530

    Consensus_simulation.js

--- Blockchain Intern/consensus_simulation.js
+++ Blockchain Intern/consensus_simulation.js
@@ -1,5 +1,4 @@
 const crypto = require('crypto');
-
 const powValidators = Array.from({ length: 5 }, (_, i) => ({
     id: `Miner${i}`,
     power: Math.floor(Math.random() * 100) + 1
@@ -8,19 +7,19 @@ const powWinner = powValidators.reduce((max, curr) => max.power > curr.power ? m
 console.log("\nProof of Work Winner:");
 console.log(powWinner);
 console.log(`Selected based on highest computational power: ${powWinner.power}`);
-console.log(`Description: Miner ${powWinner.id} will validate the next block with power ${powWinner.power} at 11:15 AM IST, June 09, 2025.`);
+console.log(`Description: Miner ${powWinner.id} will validate the next block with power ${powWinner.power}.`);
 
 const posValidators = Array.from({ length: 5 }, (_, i) => ({
     id: `Staker${i}`,
-    stake: Math.floor(Math.random() * 1000) + 1
+    stake: Math.floor(Math.random() * 1000) + 1 
 }));
 const posWinner = posValidators.reduce((max, curr) => max.stake > curr.stake ? max : curr, posValidators[0]);
 console.log("\nProof of Stake Winner:");
 console.log(posWinner);
 console.log(`Selected based on highest stake: ${posWinner.stake}`);
-console.log(`Description: Staker ${posWinner.id} will validate with a stake of ${posWinner.stake} coins at 11:15 AM IST, June 09, 2025.`);
+console.log(`Description: Staker ${posWinner.id} will validate with a stake of ${posWinner.stake} coins.`);
 
-const dposCandidates = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']; // Added two more candidates
+const dposCandidates = ['Tarun', 'Mahesh', 'Majid', 'Rampandu', 'Bhanu']; 
 const votes = Array.from({ length: 15 }, () => dposCandidates[Math.floor(Math.random() * dposCandidates.length)]); // Increased votes to 15
 const voteCounts = dposCandidates.reduce((counts, candidate) => ({
     ...counts,
@@ -30,4 +29,4 @@ const dposWinner = Object.keys(voteCounts).reduce((a, b) => voteCounts[a] > vote
 console.log("\nDelegated Proof of Stake Winner:");
 console.log(dposWinner);
 console.log(`Selected based on most community votes: ${voteCounts[dposWinner]}`);
-console.log(`Description: ${dposWinner} will serve as a delegate with ${voteCounts[dposWinner]} votes at 11:15 AM IST, June 09, 2025.`);
+console.log(`Description: ${dposWinner} will serve as a delegate with ${voteCounts[dposWinner]} votes.`);
